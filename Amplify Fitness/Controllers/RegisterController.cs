acf2f63bd90deb35feb0748cf461cf5a385f85c0
Chris Deck <cdeck20@optonline.net>
2017-10-22 23:36:02 -0400

    connecting Lambda API

--- Amplify Fitness/Controllers/RegisterController.cs
+++ Amplify Fitness/Controllers/RegisterController.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using Amazon;
+using Amazon.Lambda;
+using Amazon.Lambda.Model;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -9,9 +14,31 @@ namespace Amplify_Fitness.Controllers
     public class RegisterController : Controller
     {
         // GET: Register
-        public ActionResult Register_Home()
+        public ActionResult Register()
         {
+
+            AmazonLambdaClient client = new AmazonLambdaClient("AKIAIB2B3AH6QMWGMADA", "LMzvQzBm6bXoCEZAn8lkBv7ZH+TFtEDa6sYlKoEM", RegionEndpoint.USEast1);
+
+
+            InvokeRequest ir = new InvokeRequest
+            {
+                FunctionName = "RandomNumber",
+                InvocationType = InvocationType.RequestResponse
+            };
+
+            InvokeResponse response = client.Invoke(ir);
+
+            var sr = new StreamReader(response.Payload);
+            JsonReader reader = new JsonTextReader(sr);
+
+            var serilizer = new JsonSerializer();
+            var op = serilizer.Deserialize(reader);
+            
+            ViewData["number"] = op;
             return View();
         }
-    }
+
+       
+            
+}
 }
